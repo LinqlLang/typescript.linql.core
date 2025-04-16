427cad6f76b710ca395fae5722aa768187b0cb7c
Kris Sodroski <ksodroski@inscriptrx.org>
2025-04-16 16:20:50 -0400

    Remove combine functions from base LInqlSearch

--- projects/linql.core/src/lib/LinqlSearch.ts
+++ projects/linql.core/src/lib/LinqlSearch.ts
@@ -1,7 +1,4 @@
-import { LinqlBinary } from "./LinqlBinary";
 import { LinqlExpression } from "./LinqlExpression";
-import { LinqlFunction } from "./LinqlFunction";
-import { LinqlLambda } from "./LinqlLambda";
 import { LinqlType } from "./LinqlType";
 
 export class LinqlSearch 
@@ -35,62 +32,5 @@ export class LinqlSearch
                 this.Expressions?.push(...afterBaseType);
             }
         }
-    }
-
-    /**
-     * Combines LinqlLambda bodies with a binary AND.  Assumes there's only a single Lambda in each search.
-     * @param CombineSearch 
-     * @param FlattenTopLevelFunctions 
-     * @returns 
-     */
-    CombineAnd(CombineSearch: LinqlSearch, FlattenTopLevelFunctions: boolean = false)
-    {
-        return this.Combine(CombineSearch, FlattenTopLevelFunctions, "And");
-    }
-
-    /**
-     * Combines LinqlLambda bodies with a binary OR.  Assumes there's only a single Lambda in each search.
-     * @param CombineSearch 
-     * @param FlattenTopLevelFunctions 
-     * @returns 
-     */
-    CombineOr(CombineSearch: LinqlSearch, FlattenTopLevelFunctions: boolean = false)
-    {
-        return this.Combine(CombineSearch, FlattenTopLevelFunctions, "Or");
-    }
-
-
-    /**
-     * Combines LinqlLambda bodies with a binary defined by Combiner.  Assumes there's only a single Lambda in each search.
-     * @param CombineSearch 
-     * @param FlattenTopLevelFunctions 
-     * @param Combiner 
-     */
-    Combine(CombineSearch: LinqlSearch, FlattenTopLevelFunctions: boolean = false, Combiner: "And" | "Or")
-    {
-        const search = new LinqlSearch(this.Type);
-        search.Expressions = this.Expressions?.map(r => r.Clone());
-        let baseExpression: LinqlExpression | undefined;
-        let mergeExpression: LinqlExpression | undefined;
-        if (!FlattenTopLevelFunctions)
-        {
-            baseExpression = search.Expressions?.LastOrDefault()?.GetLastExpressionInNextChain();
-            mergeExpression = CombineSearch.Expressions?.LastOrDefault()?.GetLastExpressionInNextChain();
-        }
-        else
-        {
-            mergeExpression = search.Expressions?.slice(1).FirstOrDefault();
-            baseExpression = CombineSearch.Expressions?.slice(1).FirstOrDefault();
-        }
-
-        if (baseExpression && mergeExpression && LinqlLambda.isLinqlLambda(baseExpression) && LinqlLambda.isLinqlLambda(mergeExpression))
-        {
-            const body1 = baseExpression.Body;
-            const body2 = mergeExpression.Body;
-            const binary = new LinqlBinary(Combiner, body1, body2);
-            baseExpression.Body = binary;
-        }
-
-        return search;
     }
 }
